abc82b5bca3797e6312a187bd94bda3ef66f9c87
Programador Novato <info@programadornovato.com>
2021-12-29 18:15:45 -0600

    Crear un cargo en coinbase con PHP

--- crearCargo.php
+++ crearCargo.php
@@ -3,23 +3,24 @@ require_once './vendor/autoload.php';
 use CoinbaseCommerce\ApiClient;
 use CoinbaseCommerce\Resources\Charge;
 ApiClient::init('f14c4696-e77a-4efa-9ab4-aac045dea4e2');
-$cargo=new Charge([
-    'name'=>'Pago de acceso premium',
-    'description'=>'El cliente tiene acceso a la parte premium de la plataforma',
-    'pricing_type'=>'fixed_price',
-    'local_price'=>[
-        'amount'=>'0.20',
-        'currency'=>'USD'
+$cargo = new Charge([
+    'name' => 'Acceso al area premium',
+    'description' => 'Se tiene acceso a los videos premium',
+    'pricing_type' => 'fixed_price',
+    'local_price' => [
+        'amount' => '0.01',
+        'currency' => 'USD'
     ],
-    'metadata'=>[
-        'customer_id'=>'12345',
-        'order_id'=>'67890'
+    'metadata' => [
+        'customer_id' => '12345',
+        'order_id' => '67890'
     ]
 ]);
+
 try {
     $cargo->save();
-    echo "El pago se realizo con el id ".$cargo->id."<br>";
-    echo "Se realizara el pago en la url ".$cargo->hosted_url."<br>";
+    echo "Id del cargo=".$cargo->id."<br>";
+    echo "URL para realizar el pago=<a href='".$cargo->hosted_url."'>".$cargo->hosted_url."</a><br>";
 } catch (\Exeption $ex) {
     echo $ex->getMessage();
 }
